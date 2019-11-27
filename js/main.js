2f1c41d23bbd192605f05e3e717b2536362347f6
ola <dashevska.olga@gmail.com>
2019-11-27 13:07:39 +0200

    change notes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
+
+$('.nav-link-new').mPageScroll2id();
+
 $('.block-carousel').owlCarousel({
     loop: true,
     margin: 0,
     nav: true,
     items: 3,
-    nav: true,
     dots: false,
     responsive: {
         0: {
@@ -15,22 +17,22 @@ $('.block-carousel').owlCarousel({
     }
 });
 
-$('.slider-carousel').owlCarousel({
-    loop: true,
-    autoplay: false,
-    margin: 0,
-    items: 1,
-    nav: false,
-    dots: true,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 1
-        }
-    }
-});
+// $('.slider-carousel').owlCarousel({
+//     loop: true,
+//     autoplay: false,
+//     margin: 0,
+//     items: 1,
+//     nav: false,
+//     dots: true,
+//     responsive: {
+//         0: {
+//             items: 1
+//         },
+//         600: {
+//             items: 1
+//         }
+//     }
+// });
 
 $(function () {
     $('.toggle-menu').on('click', function () {
@@ -41,3 +43,8 @@ $(function () {
 $(document).ready(function() {
     $('.popup-link').magnificPopup();
 });
+
+$('.phoneidcl').inputmask('+38(999) 999-99-99',{ "oncomplete": function(){ 
+    $(".disbutt").prop("disabled", false);
+}, "onincomplete": function(){ $(".disbutt").prop("disabled", true); } });
+
